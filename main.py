379acb8c1726d398ce7cdff6534e3aafbf30cd82
AlexjHeat <31260270+AlexjHeat@users.noreply.github.com>
2024-12-27 13:20:13 -0800

    whoops

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from source.control import ControlWindow
-from source.db import createTables
 import source.libraries.font_lib as fontlib
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     fontlib.loadFonts()
-    createTables()
     controlWindow = ControlWindow()
 
     sys.exit(app.exec())
